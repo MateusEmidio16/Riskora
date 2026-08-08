73bee86ee93cb4132cd91e9273573c04cfc897fe
Mateus <mateusemidio418@gmail.com>
2026-08-08 23:23:25 +0100

    Adiciona o projeto base do Spring Boot

---
+++ backend/src/test/java/com/riskora/api/RiskoraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riskora.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskoraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
